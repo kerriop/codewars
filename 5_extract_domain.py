1c9081d933779574d1b66c85482173507d7b32c6
Dmitry <nor808@yandex.ru>
2021-11-22 19:49:31 +0300

    281

---
+++ 5_extract_domain.py
@@ -0,0 +1,20 @@
+from urllib.parse import urlparse
+
+
+def domain_name(url):
+    ret = urlparse(url).netloc.split('.')
+    if ret == ['']:
+        ret = urlparse(url).path
+        ret = ret.split('.')
+    print(ret)
+    if ret[0] == 'www':
+        return ret[1]
+    else:
+        return ret[0]
+
+
+# print(domain_name("http://github.com/carbonfive/raygun"))  # == "github"
+# print(domain_name("http://www.zombie-bites.com"))#== "zombie-bites"
+print(domain_name("https://www.cnet.com"))  # == "cnet"
+print(domain_name("www.xakep.ru"))  # , "xakep")
+print(domain_name("http://google.co.jp"))  # , "xakep")
